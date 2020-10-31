4a3e9a7f0a701e3f0a6a80f38ba0a74075705f31
Lukas Mertens <61001498+LukasMert@users.noreply.github.com>
2020-10-31 20:26:48 +0100

    Add files via upload

--- BirdBox/pythonscript.py
+++ BirdBox/pythonscript.py
@@ -1,35 +1,55 @@
 from picamera import PiCamera
-from gpiozero import MotionSensor
-from time import sleep
-import datetime
-
 camera = PiCamera()
+
+import time
+
+from time import sleep
+import os
+
+from gpiozero import MotionSensor
+
 pir = MotionSensor(4)
-teller = 0
+
 recording = 0
+teller = 0
+
+
+named_tuple = time.localtime()
+time_string = time.strftime("%m_%d_%Y_%Hh%M", named_tuple)
+
+
+#usb mount
+os.system("sudo mount /dev/sdb /mnt/USBdrive/")
+os.system("sudo mount /dev/sda /mnt/USBdrive/")
+print("USB mounted")
+sleep(3)
+
+from datetime import datetime
+
+teller = 0
+toggle = True
+
 
 while True:
-	while pir.motion_detected == True:
-		print("Motion detected!")
-		if recording == 0:
-			camera.start_recording("/home/pi/Birdhouse/videos/video"+"{:%Y-%m-%d %H:%M:%S}".format(datetime.datetime.now())+".h264")
-			print("Recording...")
-			recording = 1
-		if recording == 1:
-			sleep(1)
-			teller = teller + 1
-			print(teller)
-		if teller == 300 and recording == 1:
-			camera.stop_recording()
-			print("Stopped recording door teller")
-			camera.start_recording("/home/pi/Birdhouse/videos/video"+"{:%Y-%m-%d %H:%M:%S}".format(datetime.datetime.now())+".h264")
-			print("Recording again...")
-			teller = 0
-	if pir.motion_detected == False:
-		print("NO motion detected")
-		sleep(1)
-		if recording == 1:
-			camera.stop_recording()
-			print("Stopped recording")
-			recording = 0
-			teller = 0
+    if pir.motion_detected:
+        if toggle:
+            dt_string = datetime.now().strftime("%d%m%Y%H%M%S")
+            camera.start_recording('/mnt/USBdrive/movie%s.h264' %dt_string)
+            toggle = False 
+            print("recording started")
+    if pir.motion_detected == False:
+        if toggle == False:
+            camera.stop_recording()
+            print("Stopped recording")
+            teller = 0
+            toggle = True
+    if toggle == False :
+        sleep(1)
+        teller = teller + 1
+        print(teller)
+        if teller >= 30:
+            camera.stop_recording()
+            print("recording stopped")
+            teller = 0
+            toggle = True
+
